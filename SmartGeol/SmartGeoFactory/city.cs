c25a87276108daf4d2822ddfeb6e3852112f2c38
Michel Isamuna <misamuna@gmail.com>
2014-06-18 09:11:30 +0300

    update with last file by Mishka

--- SmartGeol/SmartGeoFactory/city.cs
+++ SmartGeol/SmartGeoFactory/city.cs
@@ -15,7 +15,25 @@ namespace SmartGeoFactory
         private float latcity;
         private float lngcity;
         private string cityurlimage;
+        private List<City> lstCity; //Methode pour une liste typé sous City
 
+        //Ici on va créer seulement son getter
+        public List<City> LstCity
+        {
+            get
+            {
+                if (lstCity == null) lstCity = new List<City>();
+                return lstCity;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return this.Namecity.ToString();
+        }
+
+        //Accesseurs et Mutateurs
         public int Id
         {
             get { return id; }
@@ -68,5 +86,21 @@ namespace SmartGeoFactory
             get { return cityurlimage; }
             set { cityurlimage = value; }
         }
+
+        //Methodes publiques et operations sur les objets City
+        public void Enregistrer()
+        {
+            Factory.Instance.Save(this);
+        }
+        public void Modifier()
+        {
+            Factory.Instance.Modifier(this);
+        }
+        public void Supprimer()
+        {
+            Factory.Instance.Supprimer(this);
+        }
+
+
     }
 }
